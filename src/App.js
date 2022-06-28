144447bfef1f3e37b93bcda1d9d3ea87065a009b
Padmavati Javalkar email = padmavati.javalkar@mphasis.com <padmavati.javalkar@mphasis.com>
2022-06-28 13:22:05 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import Contact from "./components/pages/Contact";
+import Post from "./components/pages/Post";
+import Layout from "./components/Layout";
+import PostLayout from "./components/PostLayout";
+import PostDetail from "./components/pages/PostDetail";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="post" element={<PostLayout />}>
+            <Route path=":category" element={<PostDetail />}/>
+            <Route index element={<Post />}/>
+          </Route>
+          <Route index element={<Home />} />
+        </Route>
+        <Route path="*" element={<h1>Error 404 Page not Found !!</h1>} />
+      </Routes>
+    </BrowserRouter>
+   </>
   );
 }
 
